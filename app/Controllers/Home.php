5dbec547dfcc68a86b053c160271280208bcbae9
OryDNA <aryapradipta967@gmail.com>
2025-06-21 13:39:28 +0700

    add api transaction unfin

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -16,6 +16,8 @@ class Home extends BaseController
             helper('form');
             helper('number');
             $this->product = new ProductModel();
+            $this->transaction = new TransactionModel();
+            $this->transaction_detail = new TransactionDetailModel();
         }
 
     public function index(): string
